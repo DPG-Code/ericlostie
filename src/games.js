58655f98a72e6ce4d5104b9b8e8e2c4b834fd534
Daniel Prieto <prietogaviriadaniel@gmail.com>
2023-05-18 11:46:20 +0200

    Initial project

---
+++ src/games.js
@@ -0,0 +1,75 @@
+export const games = [
+  {
+    image: '/añil.png',
+    title: 'Pokémon Añil',
+    description:
+      'Remake de Kanto totalmente modernizado y actualizado, una historia mejorada y la posibilidad de jugar con todos los Pokémon de las 9 gens o solo con los Pokémon clásicos.',
+    urlsDownload: {
+      url: 'https://mega.nz/file/rmhFAQSK#C5nWcB9CgaBfGmKa__XKAtKsXHbq4CgDVy7m-95MMpE',
+      urlEnglishVersion:
+        'https://drive.google.com/file/d/112KKuclpNSJ-JXDob2FhKiPKHTEViInq/view'
+    },
+    urlGuide:
+      'https://drive.google.com/drive/folders/1ViTeDaARC0R6X4SYa9RxSTQJhgMox4GC',
+    completed: true,
+    beta: false,
+    comingSoon: false
+  },
+  {
+    image: '/opalo.png',
+    title: 'Pokémon Ópalo',
+    description:
+      'Una historia ambiciosa de temática western que te llevará por 2 regiones y 16 gimnasios con el fin de evitar la destrucción del Mundo Pokémon.',
+    urlsDownload: {
+      url: 'https://mega.nz/file/jvJinLKJ#8phsB9y0T422JEYKjSnZom7fXH6bzpaxoQTV4Fkeptw',
+      urlEnglishVersion: ''
+    },
+    urlGuide:
+      'https://drive.google.com/drive/folders/1NbGZ32Izkj7WSBzkW72-1zCmO9bh7p-4',
+    completed: true,
+    beta: false,
+    comingSoon: false
+  },
+  {
+    image: 'titan.png',
+    title: 'Pokémon Titan',
+    description:
+      'El primer fangame que hice. Juego largo de 2 regiones y 16 gimnasios en el que tendrás que desmantelar a varios equipos de villanos.',
+    urlsDownload: {
+      url: 'https://www.mediafire.com/file/zvgvnrwe62m25zw/TitanFinal.zip/file',
+      urlEnglishVersion: ''
+    },
+    urlGuide: '',
+    completed: true,
+    beta: false,
+    comingSoon: false
+  },
+  {
+    image: 'iberia.png',
+    title: 'Pokémon Iberia',
+    description:
+      'Aventura llena de humor negro en una región basada en España, donde los líderes de gimnasio y los enemigos son personajes famosos de dicho país.',
+    urlsDownload: {
+      url: 'http://www.mediafire.com/file/l202209hbxynjxd/Pokemon_Iberia_V1.03.zip/file',
+      urlEnglishVersion: ''
+    },
+    urlGuide: '',
+    completed: true,
+    beta: false,
+    comingSoon: false
+  },
+  {
+    image: 'z.png',
+    title: 'Pokémon Z',
+    description:
+      'Este será mi juego definitivo. La historia tendrá lugar en la antigüedad de Kalos, donde ocurrirá la mayor guerra que el Mundo Pokémon ha presenciado.',
+    urlsDownload: {
+      url: '',
+      urlEnglishVersion: ''
+    },
+    urlGuide: '',
+    completed: false,
+    beta: false,
+    comingSoon: true
+  }
+]
